ac50a0e5e4e4176827593922af68757f4c9956cd
felix <felix.calderon89@hotmail.com>
2021-08-08 19:39:51 -0600

    mejoras

--- app/src/main/java/intcalculator/com/tel/fx/intcalculator/Result.java
+++ app/src/main/java/intcalculator/com/tel/fx/intcalculator/Result.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.text.DecimalFormat;
+
 public class Result extends AppCompatActivity {
 
     @Override
@@ -14,12 +16,16 @@ public class Result extends AppCompatActivity {
         final TextView Pendiente = findViewById(R.id.tvPendiente);
         final TextView Abonados = findViewById(R.id.tvAbonado);
         final TextView Interes = findViewById(R.id.tvInteres);
+        final TextView Total = findViewById(R.id.tvtotal);
+
+        DecimalFormat formato = new DecimalFormat("#,###.00");
 
         Bundle bundle = new Bundle();
         bundle = getIntent().getExtras();
 
-        Pendiente.setText(bundle.getString("cpendiente"));
+        Pendiente.setText(formato.format(Double.valueOf(bundle.getString("cpendiente")).doubleValue()));
         Abonados.setText(bundle.getString("cabonado"));
-        Interes.setText(bundle.getString("intereses")+"("+bundle.getString("diasintereses")+" días)");
+        Interes.setText(formato.format(Double.valueOf(bundle.getString("intereses")).doubleValue())+"("+bundle.getString("diasintereses")+" días)");
+        Total.setText(formato.format(Double.valueOf(bundle.getString("total")).doubleValue()));
     }
 }
